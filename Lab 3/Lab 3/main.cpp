5044d33020081eed03e6cfef8baa72b52ca637f4
astropeat <k.bugyi@gmail.com>
2014-05-11 14:22:55 -0700

    Lab 3

--- Lab 3/Lab 3/main.cpp
+++ Lab 3/Lab 3/main.cpp
@@ -17,119 +17,169 @@
 using namespace std;
 
 //Construct primitves
-int login();
-double member_number_entered;
-double pin_entered;
+bool login();
 int menu();
-int withdraw();
+void withdraw();
 int deposit_function();
-double deposit;
 int print();
-int quit();
-int overdraft_function();
-double overdraft_fee = 20;
+void quit();
+
+string member_name;
+double account_number;
+string pin_entered;
+double current_balance;
+
 double DAILY_WITHDRAW_THRESHOLD = 500;
-double withdrawn = 0;
+double total_withdrawn = 0;
 
-int member_number[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int pin[10]{1111, 2222, 3333, 4444, 5555, 6666, 7777, 8888, 9999, 1010};
-double balance[10]{100.00, 200.00, 300.00, 400.00, 500.00, 600.00, 700.00, 800.00, 900.00, 1000.00};
-
+int account_array[10]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+string pin_array[10]{"0000", "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999"};
 
 
 time_t currentTime = time (NULL);    // number of seconds have passed since 00h 1/1/1970
 struct tm * timeinfo = localtime ( &currentTime );
 
-void pause () {
-   long count = 0;
-   while (isspace(cin.peek()) && count != 30000099880) {
-      count++;
-
-   }
-}
 
 
 //Main function
 int main(){
-   login();
-   menu();
-
+   bool stillOperated = true;
+   if (login() == true){
+      while (stillOperated){
+         int main_response = menu();
+         switch (main_response){
+            case 1:
+               deposit_function();
+               break;
+            case 2:
+               withdraw();
+               break;
+            case 3:
+               print();
+               break;
+            case 4:
+               stillOperated = false;
+               cout << "Thank you for your business" << endl;
+               break;
+            default:
+               cout << "Please choose again.\n\n";
+               break;
+         }
+      }
+   } else {
+      return 0;
+   }
    return 0;
 }
 
 //login
-int login(){
-   cout << "Please enter member number:" << endl;
-   cin >> member_number_entered;
-   cout << "Please enter PIN:" << endl;
-   cin >> pin_entered;
+bool login(){
 
-   return 0;
+   cout << "Please enter your name: "; cin >> member_name;
+   cout << "Please enter account number: "; cin >> account_number;
+   cout << "Enter your current balance: $"; cin >> current_balance;
+   cout << "\n\nATM Bank of US" << endl;
+
+   int tries = 0;
+   while (tries < 3){
+      cout << "Please enter your pin (4-digit pin):"; cin >> pin_entered;
+
+      for (int i = 0; i <= 9; i++) {
+         if(account_number == account_array[i]){
+            if (pin_entered == pin_array[i]){
+               return true;
+            }
+         }
+      }
+      tries += 1;
+      cout << "Unable to verify PIN" << endl;
+   }
+   cout << "Too many failed attempts." << endl;
+   return false;
 }
+
+
 
 //What main calls
 int menu(){
    int main_response = 0;
+
    cout << "ATM Bank of US Menu" << endl;
    cout << " 1.  Deposit \n 2.  Withdraw \n 3.  Print Receipt \n 4.  Quit" << endl;
    cout << "Please enter an option: ";
    cin >> main_response;
 
-   if (main_response == 1){
-      deposit_function();
-   }
-   else if (main_response == 2){
-      withdraw();
-   }
-   else if (main_response == 3){
-      print();
-   }
-   else if (main_response == 4){
-      quit();
-   }
-               else cout << "Please choose again: "; cin >> main_response;
-
-
    return main_response;
 
 }
-
-/*
-int overdraft_function(){
-   if (balance - overdraft) <= 0
-
-}
-*/
 
 
 //deposit
 //add to balance, return
 
 int deposit_function(){
-   cout << "Deposit amount: ";
-   cin >> deposit;
-   pause();
+   double deposit;
+
+   cout << "\nDeposit amount: "; cin >> deposit;
    cout << "You have deposited $"; cout <<  deposit; cout << "." << endl;
-   pause();
-   return menu();
+   current_balance += deposit;
+   cout << "\n\nYour current balance is $"; cout << current_balance; cout << ".\n\n\n" << endl;
+
+   return current_balance;
 }
 
 //withdraw
 //check against max withdraw, overdraft, subtract balance, return
-int withdraw(){
-   return menu();
+void withdraw(){
+   double overdraft_fee = -20;
+   double withdrawn;
+
+   cout << "Withdrawal amount: ";
+   cin >> withdrawn;
+
+   if (total_withdrawn + withdrawn >= DAILY_WITHDRAW_THRESHOLD){
+      cout << "\nYou may not withdraw more than $500. Please try again.";
+      cout << endl;
+      return;
+   }
+   current_balance -= withdrawn;
+   total_withdrawn += withdrawn;
+
+   if (current_balance <0){
+      current_balance += overdraft_fee;
+      cout << "\nYou have overdrawn your account. A $20 fee has been applied. \n Your new balance is $"; cout << current_balance;
+      cout << "\n\n\n";
+      return;
+   }
+
+   else
+      cout << "\nYou have withdrawn $"; cout <<  withdrawn; cout << "." << endl;
+      cout << "Your current balance is $"; cout << current_balance; cout << ".\n\n\n" << endl;
+   return;
 }
 
 //print
 //prints balance receipt
 int print(){
+   cout << "\n\n*************************************************\n";
+   cout << endl;
+   cout << "\t\t\t\t\tBank of US\n" << endl;
+   cout << endl;
+   cout << "\tFor Customer Service call 1-800-111-2222\n";
+   cout << "\t\tCUSTOMER ATM TRANSACTION RECEIPT\n";
+   cout << endl;
+	cout << "\t\tDate: " << asctime (timeinfo) <<"\n";
+   cout << endl;
+   cout << "Customer name:\t\t\t\t" << member_name <<"\n";
+   cout << endl;
+   cout << "Account number:\t\t\t\t" << account_number <<"\n";
+   cout << endl;
+   cout << "Available balance:\t\t\t" << current_balance <<"\n";
+   cout << endl;
+   cout << "\tThanks for doing business with Bank of US.\n";
+   cout << "\t\t\t\tHave a great day!\n" << endl;
+   cout << "*************************************************\n";
 
-   return menu();
+   return 1;
 }
 
-//quit
-//prints thank you and terminates program
-int quit(){
-   cout << "Thank you for your business" << endl;
-   return 0;
-}
--- w5c1c2/w5c1c2/main.cpp
+++ w5c1c2/w5c1c2/main.cpp
@@ -7,6 +7,9 @@
 //
 
 #include <iostream>
+#include <iomanip>
+
+
 
 int main(int argc, const char * argv[])
 {
@@ -15,4 +18,54 @@ int main(int argc, const char * argv[])
    std::cout << "Hello, World!\n";
     return 0;
 }
+
+/*
+ 
+autofill an array:
+for (int i = 0; i < 1000; i++){
+ list[i]=-1'
+ }
+
+ 
+ passing an array to a function:
+ write a function to output the content of an integer array.
+ 1. function prototype: return type functionname (parameter list)
+ 
+ void printArray(int [], int);
+ 
+ 
+ Multiple arrays to pass in? (int[], int, int[], int....)
+
+*/
+
+//http://www.cplusplus.com/forum/beginner/60797/
+/*
+
+ void printArray(int[], int size){
+   for (int i=0; i < size; i++){
+      cout << setw(8) << list[i] << endl;
+   }
+ }
+
+ int main(){
+   int myArray[5]={3, 8, 1, 20, 70};
+ 
+   printArray(myArray, 5);
+ 
+ //pass by reference modifies the data
+ //pass by value copies the data
+
+ //pass by pointer - potentially modify the original array
+ //SOLVE THIS BY THIS: void printArray(const int [], int);
+*/
+
+
+
+
+
+
+
+
+
+
 
