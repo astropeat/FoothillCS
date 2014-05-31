ab07b5fef33c63f834fc43184b74754eb31f438a
JasonNgo97 <ngojason1997@gmail.com>
2014-05-31 12:39:29 -0700

    Update IOMSapp.h

--- CS Group Project/CS Group Project/IOMSapp.h
+++ CS Group Project/CS Group Project/IOMSapp.h
@@ -12,12 +12,24 @@
 
 #include <iostream>
 #include <string>
-
+#include "SecuritySystem.h"
+#include "ProductDatabase.h"
+#include "OrderDatabase.h"
 using namespace std;
 
 class IOMSapp
 {
 private:
+string *SS=&obj1;
+string *PD=&obj2
+SecuritySystem obj1;
+ProductDatabase obj2;
+OrderDatabase obj3;
+
+
 public:
+
+
+
 };
 #endif /* defined(__CS_Group_Project__IOMSapp__) */
