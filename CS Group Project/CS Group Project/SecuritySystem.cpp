81d71f33c361750a0ee4a444aea118d45772c397
JasonNgo97 <ngojason1997@gmail.com>
2014-05-31 10:51:32 -0700

    Update SecuritySystem.cpp

--- CS Group Project/CS Group Project/SecuritySystem.cpp
+++ CS Group Project/CS Group Project/SecuritySystem.cpp
@@ -7,3 +7,26 @@
 //
 
 #include "SecuritySystem.h"
+
+
+int SecuritySystem::checkusername(string inputusername)
+{
+  for (int x=0;x<MAXCOUNT;x++)
+  {
+    if (username[x]==inputusername)
+    {
+      index=x;
+      return 0;//return this on success
+    }
+  }
+  return -1;//return this on failure
+}
+int SecuritySystem::checkpassword(string inputpassword)
+{
+  if(inputpassword==password[index])
+  {
+    cout<<"User Authenticated"<<endl;
+    return 0;
+  }
+    return -1;//failure
+}
