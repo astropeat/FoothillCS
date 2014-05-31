959af97b8a2c8d2905cc95a8f421b8f6b7ad06c3
astropeat <k.bugyi@gmail.com>
2014-05-31 10:35:15 -0700

    updates

--- CS Group Project/CS Group Project/IOMSapp.cpp
+++ CS Group Project/CS Group Project/IOMSapp.cpp
@@ -7,3 +7,18 @@
 //
 
 #include "IOMSapp.h"
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+/*
+ Function to display a welcome message for IOMS application
+ Function to display a menu for IOMS application
+ Function for user authentication (user name and password) before he/she can access the application. This function will use the SecuritySystem pointer.
+ Function to initialize the databases. This function will invoke the buildDB from the ProductDatabase and OrderDatabase pointers
+ Function to start (run) the application: display the welcome message then display a menu for use choices (8 options). This function will need to use the ProductDatabse pointer and OrderDatabase pointer to call appropriate ProductDatabase and OrderDatabase functions depending on user selection
+ For Process Order option: ask user input for an order ID. Invoke getProductID from Order database class based on the order ID. Invoke getProduct from ProductDatabase to get a pointer to a Product object. Now invoke process order from that Product pointer. If the process order return true invoke clear from the Order
+*/
+
+
