e986cefff92295d13374af133b9152b7a2191598
astropeat <k.bugyi@gmail.com>
2014-05-31 09:32:55 -0700

    Grou Project

---
+++ CS Group Project/CS Group Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS Group Project
+//
+//  Created by Katrina Bugyi on 5/31/14.
+//  Copyright (c) 2014 Katrina Bugyi. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+   cout << "here is where we put the code" << endl;
+   return 0;
+}
+
