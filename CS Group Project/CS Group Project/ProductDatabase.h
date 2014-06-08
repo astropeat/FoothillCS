3929405c9700d0ee87664bd65adb9b83d6f334c3
astropeat <k.bugyi@gmail.com>
2014-06-08 12:17:52 -0700

    Product Database .cpp .h
    Anthony’s work

--- CS Group Project/CS Group Project/ProductDatabase.h
+++ CS Group Project/CS Group Project/ProductDatabase.h
@@ -11,15 +11,33 @@
 
 #include <iostream>
 #include <iomanip>
+#include "Product.h"
 
 using namespace std;
+
+//Data: an array of pointers (max is 10000) to Product objects, a string for the data file,
+//a static data for product count
+//Constructor: default constructor will set data file name to “products.txt”.
+//A non-default constructor will take a file name parameter
+//Destructor: de-allocate heap memory
 
 class ProductDatabase
 {
 private:
+   Product *product[10000];
+   string data_file_ = "";
+   static int product_count_;
+
 
 public:
+   ProductDatabase():data_file_("products.txt"){}; //default constructor
+   ProductDatabase(string file_name):data_file_(file_name){}; //non-default constructor
+   ~ProductDatabase() {cout<<"Product Database was destroyed..."<<endl;} //destructor
 
+   // get/set functions
+   static int getProductCount() {return product_count_;}
+   static void setProductCount(int product_count) {product_count_=product_count;}
+   void sortDatabase();
 };
 
 #endif /* defined(__CS_Group_Project__ProductDatabase__) */
