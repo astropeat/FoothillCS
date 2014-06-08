b6ebf8bb133877cdb1ac12ebb24dbc428b8ccf3c
Anthony Moore <anthony.albert.moore@gmail.com>
2014-06-08 12:08:25 -0700

    update

--- CS Group Project/CS Group Project/ProductDatabase.cpp
+++ CS Group Project/CS Group Project/ProductDatabase.cpp
@@ -12,16 +12,54 @@
 
 
 
-/*
- Functions:
- Get product count
- buildDB: reading text file to load product data into database which is an array then call the sort function to sort it by product ID.
- listing (display all product data): invoke a friend function (showProduct)  of Product class
- Add new Product: add new Product to inventory
- Discontinue Product: delete a discontinued Product (delete by Product ID)
- Stocking a Product: add more quantity to a Product
- Query a Product: display all information on a Product given a Product Id
- sort: sort the Product database by product ID
- getProduct: return a pointer to a Product given a product pointer
- resetDB: free all pointers if they’re not NULL
-*/
+
+// Functions:
+ //buildDB: reading text file to load product data into database which is an array then call the sort function to sort it by product ID.
+ 
+void buildDB (){
+
+ // ADD YOUR CODE HERE TO DECLARE VARIABLES for INPUT
+
+   ifstream fin;
+
+   fin.open (filename);
+
+   if (fin.is_open() == false) {
+      cout<<"ERROR:Failed to open file: "<<filename<<endl;
+      exit (-1);
+   }
+}
+
+//listing (display all product data): invoke a friend function (showProduct)  of Product class
+ //Add new Product: add new Product to inventory
+ //Discontinue Product: delete a discontinued Product (delete by Product ID)
+ //Stocking a Product: add more quantity to a Product
+ //Query a Product: display all information on a Product given a Product Id
+ //sort: sort the Product database by product ID
+ 
+void ProductDatabase::sortDatabase(){
+   bubbleSort(product[i], 100000);
+}
+
+ //getProduct: return a pointer to a Product given a product pointer
+ //resetDB: free all pointers if they’re not NULL
+
+void bubbleSort (int list [ ] , int size) {
+
+   int  tmp, walker;
+
+   for (int curr =0; curr < size -1; curr++){
+
+      for (walker= size-1; walker > curr; walker--) {
+         if (list[walker] < list[walker-1]) {
+            // swap
+            tmp = list[walker];
+            list[walker] = list[walker-1];
+            list[walker-1] = tmp;
+         }
+      } // end of inner loop
+
+   } // end of outer loop
+
+
+}
