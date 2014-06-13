79f0fb307ddd5c1ce938c1043d0ca083c3cdde30
astropeat <k.bugyi@gmail.com>
2014-06-12 18:45:57 -0700

    Week 10 class 2

---
+++ w10c2/w10c2/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  w10c2
+//
+//  Created by Katrina Bugyi on 6/12/14.
+//  Copyright (c) 2014 Katrina Bugyi. All rights reserved.
+//
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+   //Bubble sort
+   //http://mathbits.com/MathBits/CompSci/Arrays/Bubble.htm
+   //http://www.algolist.net/Algorithms/Sorting/Bubble_sort
+   //http://www.cprogramming.com/tutorial/computersciencetheory/sorting1.html
+
+   //Selection Sort
+   //http://mathbits.com/MathBits/CompSci/Arrays/Selection.htm
+   //http://cforbeginners.com/ssort.html
+   //http://www.learncpp.com/cpp-tutorial/64-sorting-an-array-using-selection-sort/
+
+
+
+    return 0;
+}
+
